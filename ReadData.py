f7c2bec7ccb927ea0ed409a4b60dc1f78a5ddf2b
bughunterstudios <bughunterstudios@gmail.com>
2020-10-05 21:05:02 -0600

    Get perfect dataset.

--- ReadData.py
+++ ReadData.py
@@ -16,10 +16,5 @@ for (columnName, columnData) in data.iteritems():
     print(newstring + "\t" + str(count) + "/" + str(len(columnData)) + "\t(" + str(len(columnData) - count) + ") are null")
     
 selecteddata = data[["iso_code", "continent", "location", "date", "total_cases", "new_cases", "total_deaths", "new_deaths", "stringency_index", "population", "population_density", "median_age", "aged_65_older", "aged_70_older", "gdp_per_capita", "cardiovasc_death_rate", "diabetes_prevalence", "hospital_beds_per_thousand", "life_expectancy", "human_development_index"]]
-boollist = [1] * len(selecteddata["iso_code"])
-for (columnName, columnData) in selecteddata.iteritems():
-    for i, item in enumerate(columnData):
-        if pd.isnull(item):
-            boollist[i] = 0
-print(sum(boollist))
 print(len(selecteddata["iso_code"]) - sum(selecteddata.isnull().any(axis=1)))
+perfectdata = selecteddata[-selecteddata.isnull().any(axis=1)]
