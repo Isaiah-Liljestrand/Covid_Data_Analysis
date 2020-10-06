b6d3fe91774c26cdb33bcf308cdc88aae90cfa2d
bughunterstudios <bughunterstudios@gmail.com>
2020-10-05 19:47:07 -0600

    Now using pandas. Takes less space.

--- ReadData.py
+++ ReadData.py
@@ -1,27 +1,11 @@
 # -*- coding: utf-8 -*-
-import csv
+import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
 filename = "owid-covid-data.csv"
 
-fields = [] 
-rows = []
-mydict = None
-
-with open(filename, 'r', encoding='utf') as csvfile: 
-    csvreader = csv.reader(csvfile)
-    fields = next(csvreader) 
-
-    i = 0
-    for row in csvreader: 
-        rows.append(row)
-        i += 1
-        if i > 54:
-            break
-        
-    mydict = csv.DictReader(csvfile)
-
-    print("Total no. of rows: %d"%(csvreader.line_num)) 
-
-print('Field names are:' + ', '.join(field for field in fields))
+data = pd.read_csv(filename)
+# iterating the columns 
+for col in data.columns: 
+    print(col)
