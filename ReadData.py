f285485371a5e41c14aa2dfcffe943869f4e35ae
bughunterstudios <bughunterstudios@gmail.com>
2020-10-05 20:07:47 -0600

    Processing null values.

--- ReadData.py
+++ ReadData.py
@@ -6,6 +6,11 @@ import matplotlib.pyplot as plt
 filename = "owid-covid-data.csv"
 
 data = pd.read_csv(filename)
-# iterating the columns 
-for col in data.columns: 
-    print(col)
+
+for (columnName, columnData) in data.iteritems():
+    newstring = columnName + ": "
+    count = 0
+    for i, item in enumerate(columnData):
+        if pd.notnull(item):
+            count += 1
+    print(newstring + "\t" + str(count) + "/" + str(len(columnData)) + "\t(" + str(len(columnData) - count) + ") are null")
