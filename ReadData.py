9ad7e9f5132da06ff73c749aa1fe432815d00712
Isaiah <liljestrandisaiah@gmail.com>
2020-11-30 21:59:48 -0700

    commiting code that runs wrong classification program but has no compiler errors

--- ReadData.py
+++ ReadData.py
@@ -6,6 +6,7 @@ from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from keras.models import Sequential
+from keras.layers import Dense
 
 filename = "owid-covid-data.csv"
 
@@ -23,6 +24,7 @@ selecteddata = data[["total_cases", "new_cases", "total_deaths", "new_deaths", "
 print(len(selecteddata["total_cases"]) - sum(selecteddata.isnull().any(axis=1)))
 perfectdata = selecteddata[-selecteddata.isnull().any(axis=1)]
 
+print(perfectdata)
 Y = perfectdata.pop("total_deaths")
 
 X_train, X_test, Y_train, Y_test = train_test_split(perfectdata, Y, test_size = 0.2)
@@ -36,7 +38,15 @@ model = Sequential()
 model.add(Dense(100, activation="relu", input_dim = X_train.shape[1]))
 model.add(Dense(50, activation="relu"))
 model.add(Dense(10, activation="relu"))
-model.add(Dense(y_train.shape[1], activation="softmax"))
-model.compile(loss='mean_squared_error', optimizer='adam')
+model.add(Dense(Y_train.shape[0], activation="softmax"))
+model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])
 
 model.fit(X_train, Y_train, validation_data=(X_test, Y_test), verbose = 1, epochs = 20, shuffle=True)
+predicted_valid_labels = np.argmax(model.predict(X_test), axis=1)
+valid_labels = np.argmax(Y_test, axis=1)
+
+test_range = len(valid_labels)
+for k in range(0, test_range):
+    if(predicted_valid_labels[k] == valid_labels[k]):
+        sum = sum + 1
+print("Accuracy = ", sum / test_range)
